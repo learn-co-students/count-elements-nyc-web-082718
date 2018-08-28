6f3c7ced0515f10c5062a267314fd5dddca1289a
MarSchlaefer <mschlaefer00@gmail.com>
2018-08-27 21:37:00 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,18 @@
+require 'pry'
+
 def count_elements(array)
-  # code goes here
+  # setting up new hash to hold returned values
+  new_hash = {}
+
+  array.each do |element|
+    if new_hash.include?(element) == false
+      new_hash[element] = 1
+
+      #binding.pry
+
+    elsif new_hash.include?(element)
+      new_hash[element] += 1
+    end
+  end
+  return new_hash
 end
- 
