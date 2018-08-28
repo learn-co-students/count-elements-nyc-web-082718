d55ee156ac11c0bbd1679edeaf08217901b01f82
MarSchlaefer <mschlaefer00@gmail.com>
2018-08-27 21:39:39 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -4,12 +4,14 @@ def count_elements(array)
   # setting up new hash to hold returned values
   new_hash = {}
 
+  # adding element to new_hash if it has not already been added
   array.each do |element|
     if new_hash.include?(element) == false
       new_hash[element] = 1
 
       #binding.pry
-
+      
+    # counting multiple matching elements if more than 1
     elsif new_hash.include?(element)
       new_hash[element] += 1
     end
